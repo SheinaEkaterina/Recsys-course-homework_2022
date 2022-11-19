755326ae3f36915c0e040932763dd1ffd1266751
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-11-20 00:31:12 +0300

    New FM implementation

--- task_2_fm/fm.py
+++ task_2_fm/fm.py
@@ -27,54 +27,78 @@ class FactorizationMachineTokenized(nn.Module):
         super().__init__()
         self.linear_c = CategoricalLinear(categorical_sizes)
         self.linear_n = nn.Linear(n_numerical, 1)
+        self.emb_c = Embeddings(categorical_sizes, emb_dim)
+        self.emb_n = nn.Parameter(torch.randn((n_numerical, emb_dim)))
 
     def forward(self, Xc: Tensor, Xn: Tensor) -> Tensor:
-        out_c = self.linear_c(Xc)
-        out_n = self.linear_n(Xn)
-        return torch.sigmoid(out_n + out_c)
+        linear = self.linear_c(Xc) + self.linear_n(Xn)
+
+        Xn = Xn.unsqueeze(2)
+        vx_c = self.emb_c(Xc)  # one-hot => no multiplication
+        vx_n = Xn * self.emb_n
+        vx = torch.cat([vx_c, vx_n], 1)
+        sq_sum = vx.mean(1)**2
+        sum_sq = (vx**2).mean(1)
+
+        logits = linear + 0.5 * torch.sum(sq_sum - sum_sq, 1, keepdim=True)
+        return logits
 
     @torch.no_grad()
     def predict(self, dl: DataLoader) -> np.ndarray:
         predictions = []
         for batch in tqdm(dl):
             Xc, Xn = batch[:2]
-            output = self.forward(Xc, Xn).cpu().numpy()
+            output = torch.sigmoid(self.forward(Xc, Xn)).cpu().numpy()
             predictions += list(output.ravel())
         return np.array(predictions, dtype=np.float32)
 
 
-class CategoricalLinear(nn.Module):
-    def __init__(self, categorical_sizes: Sequence[int]):
+class Embeddings(nn.Module):
+    def __init__(self, emb_numbers: Sequence[int], emb_dim: int):
         super().__init__()
-        self.embeddings = [nn.Embedding(sz, 1) for sz in categorical_sizes]
+        self.embeddings = [nn.Embedding(num, emb_dim) for num in emb_numbers]
 
     def forward(self, x: Tensor) -> Tensor:
         output = [emb(x[:, i]) for i, emb in enumerate(self.embeddings)]
-        output = torch.cat(output, 1)
-        return output.sum(1, keepdims=True)
+        return torch.stack(output, 1)
+
+
+class CategoricalLinear(Embeddings):
+    def __init__(self, categorical_sizes: Sequence[int]):
+        super().__init__(categorical_sizes, 1)
+
+    def forward(self, x: Tensor) -> Tensor:
+        output = super().forward(x)
+        return output.mean(1)
 
 
 if __name__ == "__main__":
     from tqdm import tqdm
     from torch.utils.data import DataLoader
     from sklearn.metrics import log_loss, roc_auc_score
+    from sklearn.preprocessing import StandardScaler
     from dataset import ClickDatasetTokenized
 
     ds_train = ClickDatasetTokenized("data/processed/train.csv")
-    dl_train = DataLoader(ds_train, batch_size=4096, shuffle=True)
+    scaler = StandardScaler()
+    scaler.fit(ds_train.Xn)
+    ds_train.Xn[:, :] = scaler.transform(ds_train.Xn)
+    dl_train = DataLoader(ds_train, batch_size=2048, shuffle=True)
     ds_test = ClickDatasetTokenized("data/processed/test.csv")
+    ds_test.Xn[:, :] = scaler.transform(ds_test.Xn)
     dl_test = DataLoader(ds_test, batch_size=4096, shuffle=False)
 
     model = FactorizationMachineTokenized(ds_train.cat_sizes, len(ds_train.numerical), 10)
-    loss_fn = nn.BCELoss()
-    optimizer = optim.Adam(model.parameters(), weight_decay=1e-4)
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adagrad(model.parameters(), lr=1e-1, weight_decay=1e-4)
+    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.5)
 
     for _ in range(10):
         num_iter = 0
         avg_loss = 0
         for Xc, Xn, y in tqdm(dl_train):
-            pred = model(Xc, Xn)
-            loss = loss_fn(pred, y.unsqueeze(1))
+            logits = model(Xc, Xn)
+            loss = loss_fn(logits, y.unsqueeze(1))
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -85,6 +109,7 @@ if __name__ == "__main__":
         print(f"Train loss: {avg_loss:.4f}")
 
         pred = model.predict(dl_test)
-        test_loss = log_loss(ds_test.y, pred)
+        test_loss = log_loss(ds_test.y, pred, eps=1e-7)
         test_auc = roc_auc_score(ds_test.y, pred)
         print(f"Test loss: {test_loss:.4f}, test ROC-AUC: {test_auc: .3f}")
+        scheduler.step()
