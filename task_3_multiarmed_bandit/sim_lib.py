19de25d0a49642cadc9a4b5ae680d9c992d40ae6
Худяков Юрий <catchitoo@mail.ru>
2022-12-05 01:02:18 +0500

    Add hw3

--- task_3_multiarmed_bandit/sim_lib.py
+++ task_3_multiarmed_bandit/sim_lib.py
@@ -21,9 +21,10 @@ def generate_new_banner(n, a=ALPHA, b=BETA, mu=MU, random_state=None):
     return p, lifetimes
 
 
-def simulation(policy: Callable, n=10 ** 6, initial_banners=9, seed=None):
-    state = pd.DataFrame(np.zeros((initial_banners, 4)), columns=['impressions', 'clicks', 'lifetime', 'p'])
+def simulation(policy: Callable, n=10 ** 6, initial_banners=9, seed=None, ts_explore=1.):
+    state = pd.DataFrame(np.zeros((initial_banners, 6)), columns=['impressions', 'clicks', 'lifetime', 'p', 'alpha', 'beta'])
     state['p'], state['lifetime'] = generate_new_banner(initial_banners)
+    state['beta'], state['alpha'] = 1, 1
     regret = 0
     max_index = initial_banners
     borning_rate = initial_banners*(1-np.exp(-1/MU))
@@ -32,11 +33,11 @@ def simulation(policy: Callable, n=10 ** 6, initial_banners=9, seed=None):
     for i in range(n):
         if uniform.rvs(random_state=random_state) < borning_rate or state.shape[0] < 2:
             p, lifetime = generate_new_banner(1, random_state=random_state)
-            new_banner = pd.DataFrame({'impressions': 0, 'clicks': 0, 'lifetime': lifetime, 'p': p}, index=[max_index])
+            new_banner = pd.DataFrame({'impressions': 0, 'clicks': 0, 'lifetime': lifetime, 'p': p, 'alpha': 1, 'beta': 1}, index=[max_index])
             state = pd.concat([state, new_banner], verify_integrity=True)
             max_index += 1
 
-        index = policy(state[['impressions', 'clicks']].copy())
+        index = policy(state[['impressions', 'clicks', 'alpha', 'beta']].copy())
 
         assert index in state.index, 'Error, wrong action number'
 
@@ -44,6 +45,8 @@ def simulation(policy: Callable, n=10 ** 6, initial_banners=9, seed=None):
         reward = bernoulli.rvs(p)
         state.loc[index, 'impressions'] += 1
         state.loc[index, 'clicks'] += reward
+        state.loc[index, 'alpha'] += reward
+        state.loc[index, 'beta'] += ts_explore * (1 - reward)
         regret = regret + max(state['p']) - p
 
         state['lifetime'] = state['lifetime'] - 1
