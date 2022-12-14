0487256f43598c74c4a7ce1f14d26cb593c19d04
victorialokteva <vilokteva@yandex.ru>
2022-12-14 13:06:58 -0300

    commit

--- task_4_ips/functions.py
+++ task_4_ips/functions.py
@@ -5,8 +5,8 @@ import math
 
 
 def feature_engineering(data: pd.DataFrame) -> pd.DataFrame:
-    """Немного поменяем функцию созжания фичей из 1 дз"""
-
+    """Немного поменяем функцию создания фичей из 1 дз"""
+    data = data.fillna(-1)
     # превратим date_time из строки в datetime тип данных:
     data['date_time'] = pd.to_datetime(data['date_time'])
     data['time'] = data['date_time'].dt.time
@@ -44,17 +44,8 @@ def feature_engineering(data: pd.DataFrame) -> pd.DataFrame:
 
 
 def one_hot(data):
-    cats_banner = data['banner_id'].value_counts()[lambda x: x > 200000].index
-    cats_zone = data['zone_id'].value_counts()[lambda x: x > 200000].index
-    cats_country = data['country_id'].value_counts()[lambda x: x > 300000].index
-    other_cat_features = ['os_id', 'date_copy']
-    zone_col = pd.get_dummies(pd.Categorical(data['zone_id'], categories=cats_zone)).add_prefix('zone_')
-    banner_col = pd.get_dummies(pd.Categorical(data['banner_id'], categories=cats_banner)).add_prefix('banner_')
-    country_col = pd.get_dummies(pd.Categorical(data['country_id'], categories=cats_country)).add_prefix('country_')
+    other_cat_features = ['os_id', 'country_id', 'banner_id']
     data = pd.get_dummies(data, columns=other_cat_features, drop_first=True)
-    data = pd.concat([data, zone_col], axis=1)
-    data = pd.concat([data, banner_col], axis=1)
-    data = pd.concat([data, country_col], axis=1)
     return data
 
 
@@ -65,20 +56,19 @@ def train_test_split(data: pd.DataFrame):
     features = [ 'zone_id', 'banner_id', 'os_id', 'country_id', 'date', 'time_of_day',
                  'banner_id0', 'g0', 'coeff_sum0', 'banner_id1', 'coeff_sum1']
 
-    features_0 = features + ['banner_id0']
-    features_1 = features + ['banner_id1']
     X_train = train.loc[:, features]
     Y_train = train.loc[:, 'clicks']
     # тестовая выборка с banner_id0
-    X_test0 = test.loc[:, features_0]
-    X_test0['banner_id'] = X_test0['banner_id0']
+    X_test0 = test.loc[:, features]
     # тестовая выборка с banner_id1
-
-    X_test1 = test.loc[:, features_1]
-    X_test1['banner_id'] = X_test1['banner_id1']
+    X_test1 = test.loc[:, features]
+    X_test0 = X_test0.drop(columns=['banner_id1', 'banner_id'])
+    X_test1 = X_test1.drop(columns=['banner_id', 'banner_id0'])
+    X_test1 = X_test1.rename(columns={"banner_id1": "banner_id"})
+    X_test0 = X_test0.rename(columns={"banner_id0": "banner_id"})
     Y_test = test.loc[:, 'clicks']
 
     coefs = data[['date_time', 'g0', 'g1', 'coeff_sum0', 'coeff_sum1']]
-    coefs = coefs[coefs['date_time'] >= last_day].drop(['date_time'], axis=1)
+    coefs = coefs[coefs['date_time'] >= "2021-10-02 00:00:00"].drop(['date_time'], axis=1)
 
     return X_train, Y_train, X_test0, X_test1, Y_test, coefs
